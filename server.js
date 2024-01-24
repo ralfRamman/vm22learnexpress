a1aeecdfedc168345ec92f4f656eacb5c113b1fe
opilane <opilane@tpt.lan>
2024-01-24 13:41:50 +0200

    lisasin kraami

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require(´express´);
+
+const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 const port =3000;
 
-app get (`/`,(req,res) => [
-    res.sedfile(__dirname + `/page2.html`)
-]);
+
+nunjucks.configure('views', {
+    autoescape:true,
+    express:app,
+})
+
+app.get('/',(req,res) => {
+    res.render('index.njk');
+    console.log('1');
+});
+
+
+app.get('/page2',(req,res) => {
+    res.render('page2.njk');
+    console.log('2');
+
+});
+
 
 app.listen(port,() =>(
-    console.log(`example app listening on port http://localhost:$[port]`)
+    console.log(`example app listening on port http://localhost:${port}`)
 ));
