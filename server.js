02361e2e7a97498cc9ac8024731331325be7e79a
ralf ramman <ralf.ramman@gmail.com>
2024-01-31 14:34:04 +0200

    circles

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port =3000;
 
+app.use(express.urlencoded({  
+    extended:true
+}))
+
 
 nunjucks.configure('views', {
     autoescape:true,
@@ -30,6 +34,22 @@ app.get('/form',(req,res) => {
 
 });
 
+app.get('/circle',(req,res) => {
+    res.render('circle.njk', req.query);
+})
+
+app.post('/circle', (req, res) => {
+    let area = Math.PI * req.body.radius * req.body.radius;
+    let diameter = 2 * Math.PI * req.body.radius;
+    let volume = (4/3) * Math.PI * Math.pow(req.body.radius, 3);
+    
+
+    res.render('circleAnswer.njk', { r: req.body.radius, a: area, d: diameter, v: volume });
+});
+
+
+
+
 app.listen(port,() =>(
     console.log(`example app listening on port http://localhost:${port}`)
-));
+ ));
